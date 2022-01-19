02eec9aa749b1b905b242f44cb287ab5949d4721
Piresssss <faelpiressilva@hotmail.com>
2022-01-18 21:45:18 -0300

    CRUD Clientes

---
+++ src/main/java/com/rpiress/domain/repository/ClienteRepository.java
@@ -0,0 +1,16 @@
+package com.rpiress.domain.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rpiress.domain.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+	
+	List<Cliente> findByNome(String nome);
+	List<Cliente> findByNomeContaining(String nome);
+
+}
